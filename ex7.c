0fa55c41f863116867034cf872ec6d12d0c14977
noahlz <nzucker@gmail.com>
2012-05-21 14:39:56 -0400

    finished extra credit for chapter 8 (this time actually committing the changes

--- ex7.c
+++ ex7.c
@@ -26,15 +26,39 @@ int main(int argc, char *argv[])
 	printf("Which means you should care %d%%.\n",
 			care_percentage);
 
-	printf("Printing null byte with %%s: %s \n", nul_byte);
-	printf("Printing null byte with %%c: %c \n", nul_byte);
+	// printf("Printing null byte with %%s: %s \n", nul_byte);
+	// printf("Printing null byte with %%c: %c \n", nul_byte);
 
 	// EXTRA CREDIT
 	// 1) various sizes of universe_of_defects
+	//    ANSWER: "warning: integer constant is too large for its type" 
 	// 2) what do these huge numbers actually print out?
+	//    ANSWER: the huge number prints as 0.  part_of_universe_prints as "inf"    
 	// 3) Change long to unsigned long and find the number that's too big
+	//    ANSWER: the expression  
+	//    unsigned long universe_of_defects = 100000000000L * 1024L * 1024L * 1024L; 
+	//    has compiler error "warning: integer overflow in expression" and causes 
+	//    later expressions to overflow            
 	// 4) Understand what unsigned does
+	//    ANSWER: Expands the maximum value that can be stored in a variable
+	//    by removing the negative range.  See below:
+	
+	long foo = -10000L;	
+	unsigned long bar = -10000L;
+	printf(">> Extra credit 4:\n");
+	printf("\tfoo = %ld\n", foo);	
+	printf("\tbar = %lu\n", bar); // http://stackoverflow.com/q/2844/7507	
+	
 	// 5) explain to yourself why you can multiply char and int.
+	// char is merely the numeric code for a character that can be displayed
+	// on screen or printed to hard copy. As such, it is subject to
+	// arithmetic. Another way of thinking about it is that a char value is a
+	// pointer to an entry in a character table, and - like pointers - is subject
+	// to arithmetic.
+		
 
+	///////////////////////////////////////////////////////////////////////////
+    // END EXTRA CREDIT
+	
 	return 0;
 }
