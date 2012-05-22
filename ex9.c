d04f89256d75a018d004c155baf6d02a272ca734
noahlz <nzucker@gmail.com>
2012-05-22 16:11:16 -0400

    Exercise 9, extra credits 1-4

--- ex9.c
+++ ex9.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
 	name[0] = 'Z';
 	name[1] = 'e';
 	name[2] = 'd';
-	name[3] = 'A';
+	name[3] = '\0';
 
 	// then print them out intialized
 	printf("numbers: %d %d %d %d\n",
@@ -67,18 +67,78 @@ int main(int argc, char *argv[])
 	
 	// 1) Assign characters to numbers and then use printf to print them a
 	// character at at time.
+	// ANSWER: I didn't get any compiler errors. Printed out the following:
+	// 	   numbers containing characters:
+	// 	   97 98 99 100
+	// (tried with %c format string too)
+	numbers[0] = 'a';
+	numbers[1] = 'b';
+	numbers[2] = 'c';
+	numbers[3] = 'd';
+	printf("numbers containing characters:\n %d %d %d %d\n",
+			numbers[0], numbers[1], numbers[2], numbers[3]);
 	
 	// 2) Do the inverse of (1) for name, trying to treat it like an array of
 	// int and print out one int at a time. What does Valgrind think of this?
+	// ANSWER: Again, no compiler errors. Valgrind doesn't care. Prints out the following:
+	//     name containing numbers:
+ 	//     a b c d
+ 	// (tried with %i format string too)
+	name[0] = 97;
+	name[1] = 98;
+	name[2] = 99;
+	name[3] = 100;
+	printf("name containing numbers:\n %c %c %c %c\n", 
+			name[0], name[1], name[2], name[3]);
+
+	// 3) How many other ways can you print this out?
 	
-	// 3) How many other ways can you print this out.
+	printf("name as octal:\n %o %o %o %o\n",
+			name[0], name[1], name[2], name[3]);
+
+	// This line gets the following compiler warning:
+	// "format ‘%e’ expects type ‘double’, but argument 2 has type ‘int’"
+	// Valgrind has many errors including this one:
+	//    "Use of uninitialised value of size 8"
+	// Indicating that the double is 8 bytes wide but the char value referenced
+	// is much smaller?
+	printf("name as double:\n %e %e %e %e\n",
+			name[0], name[1], name[2], name[3]);
+	printf("size of int: %ld\n", sizeof(int)); // 4
+	printf("size of char: %ld\n", sizeof(char)); // 1
+	printf("size of number[] element: %ld\n", sizeof(numbers[0])); // 4
+	printf("size of name[] element: %ld\n", sizeof(name[0])); // 1
+	printf("size of double: %ld\n", sizeof(double)); // 8
 	
 	// 4) If an array of characters is 4 bytes long, and an integer is 4 bytes
-	// long, then you can you treat the whole tname array like it's an
-	// integer?
+	// long, then you can you treat the whole name array like it's an integer?
+	
+	printf("size of name: %ld\n", sizeof(name));
+	
+	// This isn't the answer, it prints out a different value each run,
+	// meaning that casting it gets you the memory address, not the "number"
+	// stored in the array.
+	printf("name's memory address: %lld\n", (long long int) name);
+	
+	// I did various searches for "covert char array to int" but many of the
+	// results had to do with "string-to-int" coverstion and the function 'atoi'
+	char c[4];
+	c[0] = 'Z';
+	c[1] = 'e';
+	c[2] = 'd';
+	c[3] = '\0';
+	printf("chars as hex bytes: %X %X %X %X\n", c[0], c[1], c[2], c[3]);
+	// How to get these into an integer? Bitwise manipulation.
+	unsigned int num;
+	num = c[0] << 24;
+	num = (c[1] << 16) | num;
+	num = (c[2] << 8) | num;
+	num = c[3] | num;
+	printf("char array as hexidecimal: %X\n", num);	
+	printf("char array as integer: %d\n", num);
 	
 	// 5) Take a piece of paper and draw out each of these arrays as a row of
-	// boxes. Then code the operations you just did on paper to see if you go
+	// boxes. Then code the operations you just did on paper to see if you got
 	// them right.
 	
 	// 6) Convert name to be in the style of another and see if the code keeps
